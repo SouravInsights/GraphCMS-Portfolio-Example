c86c06c5e58ddbe90b9c248640e8a7f846e01841
souravinsights <souravkumarnanda4@gmail.com>
2020-08-15 15:25:28 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -44,42 +44,41 @@ const Footer = () => {
           Hope you're doing great!
         </Heading>
       </Box>
-      <Box zIndex="1">
-        <Box w={["300px", "350px", "400px", "400px"]} textAlign="center">
-          <List d="flex" justifyContent="space-around">
-            <ListItem d="inline-block" cursor="pointer">
-              <ListIcon>
-                <FaFacebook color="#181818" size="1.5em" />
-              </ListIcon>
-            </ListItem>
-            <ListItem d="inline-block" cursor="pointer">
-              <ListIcon>
-                <FaTwitter color="#181818" size="1.5em" />
-              </ListIcon>
-            </ListItem>
-            <ListItem d="inline-block" cursor="pointer">
-              <ListIcon>
-                <FaDribbble color="#181818" size="1.5em" />
-              </ListIcon>
-            </ListItem>
-            <ListItem d="inline-block" cursor="pointer">
-              <ListIcon>
-                <FaGithub color="#181818" size="1.5em" />
-              </ListIcon>
-            </ListItem>
-            <ListItem d="inline-block" cursor="pointer">
-              <ListIcon>
-                <FaLinkedin color="#181818" size="1.5em" />
-              </ListIcon>
-            </ListItem>
-          </List>
-        </Box>
+      <Box display="flex" flexDirection="column" zIndex="1">
+        <List d="flex" justifyContent="space-around">
+          <ListItem d="inline-block" cursor="pointer">
+            <ListIcon>
+              <FaFacebook color="#181818" size="1.5em" />
+            </ListIcon>
+          </ListItem>
+          <ListItem d="inline-block" cursor="pointer">
+            <ListIcon>
+              <FaTwitter color="#181818" size="1.5em" />
+            </ListIcon>
+          </ListItem>
+          <ListItem d="inline-block" cursor="pointer">
+            <ListIcon>
+              <FaDribbble color="#181818" size="1.5em" />
+            </ListIcon>
+          </ListItem>
+          <ListItem d="inline-block" cursor="pointer">
+            <ListIcon>
+              <FaGithub color="#181818" size="1.5em" />
+            </ListIcon>
+          </ListItem>
+          <ListItem d="inline-block" cursor="pointer">
+            <ListIcon>
+              <FaLinkedin color="#181818" size="1.5em" />
+            </ListIcon>
+          </ListItem>
+        </List>
+
         <Text
-          textAlign="center"
           fontWeight="bold"
           fontSize={["sm", "sm", "sm", "sm"]}
           color="gray.50"
           mb={4}
+          textAlign="center"
         >
           Copyright @ 2020 | SouravInsights. Please don't fork me!
         </Text>

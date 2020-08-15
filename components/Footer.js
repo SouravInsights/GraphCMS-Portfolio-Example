0770a4759a23dea2425dad18eb8025a872afe381
souravinsights <souravkumarnanda4@gmail.com>
2020-08-15 14:31:10 +0530

    Footer updated, some fixes peding

--- components/Footer.js
+++ components/Footer.js
@@ -1,74 +1,90 @@
-import { Button, Flex, Heading, Box, Text, List, ListItem, ListIcon } from '@chakra-ui/core';
-import React from 'react';
-import { motion } from "framer-motion";
 import {
-    FaFacebook,
-    FaTwitter,
-    FaDribbble,
-    FaGithub,
-    FaLinkedin
-} from 'react-icons/fa';
+  Button,
+  Flex,
+  Heading,
+  Box,
+  Text,
+  List,
+  ListItem,
+  ListIcon,
+} from "@chakra-ui/core";
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "../styles/styles.module.css";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaDribbble,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const ContactButton = motion.custom(Button);
 
 const Footer = () => {
-    return (
+  return (
+    <Flex
+      bg="white"
+      px={["30px", "30px", "80px", "140px"]}
+      py={["20px", "20px", "20px", "20px"]}
+      justifyContent={["none", "none", "space-between", "space-between"]}
+      flexDirection={["column", "column", "row", "row"]}
+      className={styles.footer}
+    >
+      <Box zIndex="1">
+        <Text
+          fontSize="18px"
+          fontWeight="medium"
+          letterSpacing="0.1em"
+          color="gray.50"
+        >
+          THANKS FOR STOPPING BY
+        </Text>
+        <Heading fontSize="2xl" color="gray.50" fontWeight="extrabold">
+          Hope you're doing great!
+        </Heading>
+      </Box>
+      <Box zIndex="1">
+        <Box w={["300px", "350px", "400px", "400px"]} textAlign="center">
+          <List d="flex" justifyContent="space-around">
+            <ListItem d="inline-block" cursor="pointer">
+              <ListIcon>
+                <FaFacebook color="#181818" size="1.5em" />
+              </ListIcon>
+            </ListItem>
+            <ListItem d="inline-block" cursor="pointer">
+              <ListIcon>
+                <FaTwitter color="#181818" size="1.5em" />
+              </ListIcon>
+            </ListItem>
+            <ListItem d="inline-block" cursor="pointer">
+              <ListIcon>
+                <FaDribbble color="#181818" size="1.5em" />
+              </ListIcon>
+            </ListItem>
+            <ListItem d="inline-block" cursor="pointer">
+              <ListIcon>
+                <FaGithub color="#181818" size="1.5em" />
+              </ListIcon>
+            </ListItem>
+            <ListItem d="inline-block" cursor="pointer">
+              <ListIcon>
+                <FaLinkedin color="#181818" size="1.5em" />
+              </ListIcon>
+            </ListItem>
+          </List>
+        </Box>
+        <Text
+          textAlign="center"
+          fontWeight="bold"
+          fontSize={["sm", "sm", "sm", "sm"]}
+          color="gray.50"
+        >
+          Copyright @ 2020 | SouravInsights. Please don't fork me!
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
 
-        <Flex bg="#090909" px={["20px", "40px", "60px", "60px"]} flexDirection="column" align="center" justify="center">
-            <Heading fontSize={["2xl", "4xl", "5xl", "6xl"]} mt={["20px", null, null, "38px"]} fontWeight="extrabold" textAlign="center" color="white">
-                Build products people love.
-                </Heading>
-            <ContactButton
-                mt={["30px", null, null, "42px"]}
-                h={["47px", "74px"]}
-                w={["180px", "300px"]}
-                bg="#0099FF"
-                fontSize={["xl", "2xl", "3xl", "3xl"]}
-                fontWeight="bold"
-                color="white"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 1 }}
-                outline="none"
-            >
-                Let's Talk
-                </ContactButton>
-            <Box display="flex" my="34px" flexDirection="column" alignItems="center">
-                <Box w={['300px', '350px', '400px', '500px']} textAlign="center">
-                    <List d="flex" justifyContent="space-around">
-                        <ListItem d="inline-block" cursor="pointer">
-                            <ListIcon>
-                                <FaFacebook color="white" size="1.5em" />
-                            </ListIcon>
-                        </ListItem>
-                        <ListItem d="inline-block" cursor="pointer">
-                            <ListIcon>
-                                <FaTwitter color="white" size="1.5em" />
-                            </ListIcon>
-                        </ListItem>
-                        <ListItem d="inline-block" cursor="pointer">
-                            <ListIcon>
-                                <FaDribbble color="white" size="1.5em" />
-                            </ListIcon>
-                        </ListItem>
-                        <ListItem d="inline-block" cursor="pointer">
-                            <ListIcon>
-                                <FaGithub color="white" size="1.5em" />
-                            </ListIcon>
-                        </ListItem>
-                        <ListItem d="inline-block" cursor="pointer">
-                            <ListIcon>
-                                <FaLinkedin color="white" size="1.5em" />
-                            </ListIcon>
-                        </ListItem>
-                    </List>
-                </Box>
-                <Text color="white" textAlign="center" fontWeight="bold" fontSize={['sm', 'sm', 'lg', 'lg']}>
-                    Copyright @ 2020 | SouravInsights. <br /> Please don't fork me!
-                </Text>
-            </Box>
-        </Flex >
-
-    )
-}
-
-export default Footer
+export default Footer;
